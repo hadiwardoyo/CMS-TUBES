0d678a4ef92b21222e76686f4ce4b9693bd8099d
hadiwardoyo <agungrah11@gmail.com>
2023-12-01 13:49:34 +0700

    fixing product and more

--- src/helpers/MasterAPI.js
+++ src/helpers/MasterAPI.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:3000";
-// export const URL = "https://dfresh.artesia.id";
+// export const URL = "http://localhost:3000";
+export const URL = "https://dfresh.artesia.id";
